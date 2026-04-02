b288c7c8cd749076f5e1c4abb0d4172a548ec86d
PeterBean <peterbean410@gmail.com>
2026-04-02 15:16:47 +0000

    Remove marketdata/ prefix from the DAG script path

--- src/marketdata/dags/download_interval_price_data_dag.py
+++ src/marketdata/dags/download_interval_price_data_dag.py
@@ -33,7 +33,7 @@ with DAG(
         image="731833471586.dkr.ecr.ap-southeast-1.amazonaws.com/forex-marketdata-download-interval-price-data:202604011539",
         image_pull_policy="IfNotPresent",
         image_pull_secrets=[k8s.V1LocalObjectReference(name="ecr-registry-credentials")],
-        cmds=["python", "marketdata/usecases/download-interval-price-data.py"],
+        cmds=["python", "usecases/download-interval-price-data.py"],
         env_vars=[
             k8s.V1EnvVar(name="FX_SYMBOL", value="USDJPY"),
             k8s.V1EnvVar(name="FX_INTERVAL", value="15"),
